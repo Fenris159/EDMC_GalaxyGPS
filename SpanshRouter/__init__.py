2b521035f0a5047174fa974ea96b92bc2b59c6c8
Fenris159 <drew.brdly78@gmail.com>
2026-01-20 19:20:37 -0600

    removed old directories

--- SpanshRouter/__init__.py
+++
@@ -1,7 +0,0 @@
-import sys
-import traceback
-from .updater import SpanshUpdater
-from .AutoCompleter import AutoCompleter
-from .PlaceHolder import PlaceHolder
-from .FleetCarrierManager import FleetCarrierManager
-from .GalaxyGPS import GalaxyGPS
